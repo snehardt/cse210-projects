251df67394bb2b00ae4d1dcb04121d630aea2158
snehardt <spencer.ehardt@gmail.com>
2026-01-09 13:07:01 -0700

    finished prep 2, with letter grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is you grade percetage?");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >=70)
+        {
+            letter = "C";
+        }
+        else if (grade >=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is a {letter}.");
+
+        if (letter == "A" || letter == "B" || letter == "C")
+        {
+            Console.WriteLine("Congradulations! You have passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, better luck next time.");
+        }
     }
 }
