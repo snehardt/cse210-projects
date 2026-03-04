7e59912344aa3112c529120c34a358016322de7a
snehardt <spencer.ehardt@gmail.com>
2026-03-04 12:22:17 -0700

    added shapes.cs

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Purple", 5);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Orange", 4, 7);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Green", 3);
+        shapes.Add(shape3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
